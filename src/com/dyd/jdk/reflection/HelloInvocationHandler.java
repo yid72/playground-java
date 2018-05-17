5852146dd44135320bae7732262525874cfb1e69
Du <yidongdu@amazon.com>
2018-05-17 10:30:40 -0700

    test cases for suppressed exceptions

---
+++ src/com/dyd/jdk/reflection/HelloInvocationHandler.java
@@ -0,0 +1,15 @@
+package com.dyd.jdk.reflection;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class HelloInvocationHandler implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy,
+                  Method method,
+                  Object[] args)
+            throws Throwable {
+        System.out.println("hello ");
+        return null;
+    }
+}
---
+++ tst/com/dyd/jdk/reflection/ProxyTest.java
@@ -0,0 +1,20 @@
+package com.dyd.jdk.reflection;
+
+import org.junit.Test;
+
+import java.lang.reflect.Proxy;
+
+public class ProxyTest {
+    @Test
+    public void test() {
+        HelloInvocationHandler handler = new HelloInvocationHandler();
+
+        Hello proxy = (Hello) Proxy.newProxyInstance(
+                ProxyTest.class.getClassLoader(),
+                new Class[] {Hello.class},
+                handler
+        );
+
+        proxy.sayHello();
+    }
+}
