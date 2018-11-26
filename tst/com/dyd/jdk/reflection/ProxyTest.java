e25a23ec6d4e1b67a41a26fc232a17f816f05455
yid72 <duyidong@gmail.com>
2018-11-25 18:52:35 -0800

    use junit 5

--- tst/com/dyd/jdk/reflection/ProxyTest.java
+++ tst/com/dyd/jdk/reflection/ProxyTest.java
@@ -1,6 +1,6 @@
 package com.dyd.jdk.reflection;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Proxy;
 
--- tst/com/dyd/jdk/suppressed/HelloWorldTest.java
+++ tst/com/dyd/jdk/suppressed/HelloWorldTest.java
@@ -1,6 +1,6 @@
 package com.dyd.jdk.suppressed;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
